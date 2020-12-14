387225b45202dc55285717a3f46d80238f4c674d
SC136 <swarchuri06@gmail.com>
2020-12-14 21:44:49 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,41 @@
+const { Client } = require('discord.js');
+const { play, stop, help} = require('./commands');
+
+const bot = new Client();
+
+bot.login('NzgwODM4NzA4NjY0NDY3NDU2.X706tw.ffi28PFilfRlL-vu1XyFQt1ZzOw');
+
+bot.on('ready', () => {
+  console.log('Bot has logged in and is ready !');
+  bot.user.setActivity("Simplicity | Type .help", { type: "LISTENING" });
+
+});
+
+bot.on('message', (msg) => {
+    if (msg.author.bot) return;
+
+    const prefix = '.';
+    if (!msg.content.startsWith(prefix)) return;
+
+    const commandName = getCommandName(prefix, msg.content);
+    const args = getCommandArgs(prefix, msg.content);
+
+    if (commandName === 'play')
+      return play(msg, args);
+    else if  (commandName === 'stop')
+      return stop(msg, args);
+    else if  (commandName === 'help')
+      return help(msg, args); 
+});
+
+function getCommandName(prefix, content) {
+    return content 
+    .slice(prefix.length)
+    .split(' ')[0];
+}
+function getCommandArgs(prefix, content) {
+    return content
+      .slice(prefix.length)
+      .split(' ')
+      .slice(1);
+}
---
+++ commands.js
@@ -0,0 +1,51 @@
+const downloadYT = require('ytdl-core');
+const searchYT = require('yt-search');
+const Discord = require("discord.js");
+async function play(msg, ...args) {
+    
+          if (!msg.member.voice.channel) {
+            let errorEmbed = new Discord.MessageEmbed()
+                .setDescription('You need to be in a voice channel to play music!')
+            	.setFooter(`Requested by: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
+            return msg.channel.send(errorEmbed);
+        }
+  if (!args.length) return msg.reply("Please give a song name");
+  const vc = msg.member.voice.channel;
+  const connection = await vc.join();
+  const video = await findVideo(args.join(' '));
+
+  if (video) {
+      const stream = downloadYT(video.url, {filter: 'audioonly'});
+      connection.play(stream, { seek: 0, volume: 1 });
+
+      await msg.reply(`Now playing \`${video.title}\`.`);
+  } else
+    await msg.reply(`You need to enter a song name!`);
+}
+async function findVideo(query) {
+    if (!query.length) return;
+    const result = await searchYT(query);
+    return (result.videos.length > 1)
+    ? result.videos[0]
+    : null;
+}
+
+async function stop(msg) {
+            if (!msg.member.voice.channel) {
+            let errorEmbed = new Discord.MessageEmbed()
+                .setDescription('You need to be in a voice channel to stop music!')
+            	.setFooter(`Requested by: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
+            return msg.channel.send(errorEmbed);
+        }
+  const vc = msg.member.voice.channel;
+  await vc.leave();
+
+  await msg.reply('Stopped.');
+}
+async function help(msg) {
+  await msg.reply('***Help Command*** \n\n`.play <songname>` \n\n`.stop` \n\nSo Simple! \n\n*If You Need Any Help Join The Support Server* : https://discord.gg/Qysc2PXp5e');
+}
+
+module.exports.play = play;
+module.exports.stop = stop;
+module.exports.help = help;
