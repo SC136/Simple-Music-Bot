6c5953fd8181d67ad6fc03b67dac95b07aec0865
SC136 <swarchuri06@gmail.com>
2020-12-20 17:24:35 +0530

    updated ready bot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = new Client();
 bot.login('NzgwODM4NzA4NjY0NDY3NDU2.X706tw.ffi28PFilfRlL-vu1XyFQt1ZzOw');
 
 bot.on('ready', () => {
-  console.log('Bot has logged in and is ready !');
+  console.log('Bot has logged in and is playing music!');
   bot.user.setActivity("Simplicity | Type .help", { type: "LISTENING" });
 
 });
