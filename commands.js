f770005a117d37c5338ffcb863431ce49ed335cc
SC136 <swarchuri06@gmail.com>
2021-02-05 18:06:23 +0530

    Error Handler Added!

--- commands.js
+++ commands.js
@@ -1,44 +1,49 @@
 const downloadYT = require('ytdl-core');
 const searchYT = require('yt-search');
-const { Discord, MessageEmbed, Client } = require("discord.js");
+const { Discord, MessageEmbed, Client, VoiceChannel } = require("discord.js");
 const bot = new Client();
 const moment = require('moment');
+
 async function play(msg, ...args) {
-    
-          if (!msg.member.voice.channel) {
-            let errorEmbed = new MessageEmbed()
-                .setDescription('You Need To Be In A Voice Channel To Play The Music!')
-            	.setFooter(`Requested By: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
-            return msg.channel.send(errorEmbed);
-        }
+
+  if (!msg.member.voice.channel) {
+    let errorEmbed = new MessageEmbed()
+      .setDescription('You Need To Be In A Voice Channel To Play The Music!')
+      .setFooter(`Requested By: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
+    return msg.channel.send(errorEmbed);
+  }
+
+  const permissions = VoiceChannel.permissionsFor(message.bot.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) return msg.reply("I Am Missing Permissions!")
+
   if (!args.length) return msg.reply("Please Give A Song Name!");
   const vc = msg.member.voice.channel;
   const connection = await vc.join();
   const video = await findVideo(args.join(' '));
 
   if (video) {
-      const stream = downloadYT(video.url, {filter: 'audioonly'});
-      connection.play(stream, { seek: 0, volume: 1 });
+    const stream = downloadYT(video.url, { filter: 'audioonly' });
+    connection.play(stream, { seek: 0, volume: 1 });
 
-      await msg.reply(`Now Playing \`${video.title}\`.`);
+    await msg.reply(`Now Playing \`${video.title}\`.`);
   } else
     await msg.reply(`You Need To Enter A Valid Song Name!`);
 }
 async function findVideo(query) {
-    if (!query.length) return;
-    const result = await searchYT(query);
-    return (result.videos.length > 1)
+  if (!query.length) return;
+  const result = await searchYT(query);
+  return (result.videos.length > 1)
     ? result.videos[0]
     : null;
 }
 
 async function stop(msg) {
-            if (!msg.member.voice.channel) {
-            let errorEmbed = new MessageEmbed()
-                .setDescription('You Need To Be In A Voice Channel To Stop The Music!')
-            	.setFooter(`Requested by: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
-            return msg.channel.send(errorEmbed);
-        }
+  if (!msg.member.voice.channel) {
+    let errorEmbed = new MessageEmbed()
+      .setDescription('You Need To Be In A Voice Channel To Stop The Music!')
+      .setFooter(`Requested by: ${msg.author.tag}`, msg.author.avatarURL({ "format": "png" }))
+    return msg.channel.send(errorEmbed);
+  }
   const vc = msg.member.voice.channel;
   await vc.leave();
 
@@ -52,9 +57,9 @@ async function ping(msg) {
 }
 async function server(msg) {
   const embed = new MessageEmbed()
-  .setTitle('Simple Music Bot Support Server Invite Link :')
-  .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
-  .setDescription('Click [Here](https://discord.gg/Qysc2PXp5e) To Join The Support Server')
+    .setTitle('Simple Music Bot Support Server Invite Link :')
+    .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
+    .setDescription('Click [Here](https://discord.gg/Qysc2PXp5e) To Join The Support Server')
   await msg.channel.send(embed);
 }
 async function servers(msg) {
@@ -65,39 +70,39 @@ async function users(msg) {
 }
 async function invite(msg) {
   const invite = new MessageEmbed()
-  .setTitle('Simple Music Bot Invite Link :')
-  .setDescription('[Click Here To Invite Simple Music Bot](https://discord.com/api/oauth2/authorize?client_id=780838708664467456&permissions=3147776&scope=bot)')
-  .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
+    .setTitle('Simple Music Bot Invite Link :')
+    .setDescription('[Click Here To Invite Simple Music Bot](https://discord.com/api/oauth2/authorize?client_id=780838708664467456&permissions=3147776&scope=bot)')
+    .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
   await msg.channel.send(invite)
 }
 async function vote(msg) {
   const vote = new MessageEmbed()
-  .setDescription('If You Use Simple Music Bot & You Like It,\nThen Consider Voting The Bot In One Of These List!')
-  .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
-  .setTitle('You Can Vote Simple Music Bot Here In These Lists :')
-  .addField('Top.gg', `[Vote!](https://top.gg/bot/780838708664467456/vote "Vote On Top.gg! (Recommended)") (Recommended)`)
-  .addField('Discord Boats', `[Vote!](https://discord.boats/bot/780838708664467456/vote "Vote On Discord Boats!")`)
-  .addField('Bots For Discord', `[Vote!](https://botsfordiscord.com/bot/780838708664467456/vote "Vote For Bots For Discord!")`)
-  .addField('Discord Bot List',`[Vote!](https://discordbotlist.com/bots/simple-music-bot/upvote "Vote On Discord Bot List!")`)
-  .setFooter('Tip : You Can Vote Daily!')
-  .setColor(msg.guild.me.displayHexColor)
+    .setDescription('If You Use Simple Music Bot & You Like It,\nThen Consider Voting The Bot In One Of These List!')
+    .setThumbnail('https://images.discordapp.net/avatars/780838708664467456/edc00a67ea08480b50497fb1b6fe10a8.png?size=512')
+    .setTitle('You Can Vote Simple Music Bot Here In These Lists :')
+    .addField('Top.gg', `[Vote!](https://top.gg/bot/780838708664467456/vote "Vote On Top.gg! (Recommended)") (Recommended)`)
+    .addField('Discord Boats', `[Vote!](https://discord.boats/bot/780838708664467456/vote "Vote On Discord Boats!")`)
+    .addField('Bots For Discord', `[Vote!](https://botsfordiscord.com/bot/780838708664467456/vote "Vote For Bots For Discord!")`)
+    .addField('Discord Bot List', `[Vote!](https://discordbotlist.com/bots/simple-music-bot/upvote "Vote On Discord Bot List!")`)
+    .setFooter('Tip : You Can Vote Daily!')
+    .setColor(msg.guild.me.displayHexColor)
   await msg.channel.send(vote)
 }
 async function uptime(msg) {
   const d = moment.duration(msg.client.uptime);
-    const days = (d.days() == 1) ? `${d.days()} Day` : `${d.days()} Days`;
-    const hours = (d.hours() == 1) ? `${d.hours()} Hour` : `${d.hours()} Hours`;
-    const minutes = (d.minutes() == 1) ? `${d.minutes()} Minute` : `${d.minutes()} Minutes`;
-    const seconds = (d.seconds() == 1) ? `${d.seconds()} Second` : `${d.seconds()} Seconds`;
-    const date = moment().subtract(d, 'ms').format('dddd, MMMM Do YYYY');
-    const embed = new MessageEmbed()
-      .setTitle('Simple Music Bot Uptime :')
-      .setThumbnail('')
-      .setDescription(`\`\`\`prolog\n${days}, ${hours}, ${minutes}, and ${seconds}\`\`\``)
-      .addField('Date Launched :', date) 
-      .setFooter(msg.member.displayName,  msg.author.displayAvatarURL({ dynamic: true }))
-      .setTimestamp()
-      .setColor(msg.guild.me.displayHexColor);
+  const days = (d.days() == 1) ? `${d.days()} Day` : `${d.days()} Days`;
+  const hours = (d.hours() == 1) ? `${d.hours()} Hour` : `${d.hours()} Hours`;
+  const minutes = (d.minutes() == 1) ? `${d.minutes()} Minute` : `${d.minutes()} Minutes`;
+  const seconds = (d.seconds() == 1) ? `${d.seconds()} Second` : `${d.seconds()} Seconds`;
+  const date = moment().subtract(d, 'ms').format('dddd, MMMM Do YYYY');
+  const embed = new MessageEmbed()
+    .setTitle('Simple Music Bot Uptime :')
+    .setThumbnail('')
+    .setDescription(`\`\`\`prolog\n${days}, ${hours}, ${minutes}, and ${seconds}\`\`\``)
+    .addField('Date Launched :', date)
+    .setFooter(msg.member.displayName, msg.author.displayAvatarURL({ dynamic: true }))
+    .setTimestamp()
+    .setColor(msg.guild.me.displayHexColor);
   await msg.channel.send(embed);
 }
 
