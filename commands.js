c06fd6cf849b830778e5bba6116af10d8af3108d
SC136 <swarchuri06@gmail.com>
2021-02-17 12:16:00 +0530

    Simple Music Bot

--- commands.js
+++ commands.js
@@ -20,14 +20,15 @@ async function play(msg, ...args) {
   if (!args.length) return msg.reply("Please Give A Song Name!");
   const vc = msg.member.voice.channel;
   const connection = await vc.join()
-    .then(connection => {
-      connection.voice.setServerDeaf(true);
-    });
+
   const video = await findVideo(args.join(' '));
 
   if (video) {
     const stream = downloadYT(video.url, { filter: 'audioonly' });
-    connection.play(stream, { seek: 0, volume: 1 });
+    connection.play(stream, { seek: 0, volume: 1 })
+      .then(connection => {
+        connection.voice.setSelfDeaf(true);
+      });
 
     await msg.reply(`Now Playing \`${video.title}\`.`);
   } else
