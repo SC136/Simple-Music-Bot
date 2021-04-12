3a4a1cf20d4ed2b796adfd2b32143ac377ce0309
SC136 <swarchuri06@gmail.com>
2021-04-12 15:13:33 +0530

    Simple Music Bot

---
+++ commands/eval.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'eval',
+    run: async (message, args, client) => {
+        if (message.author.id !== '594504468931018752') return message.channel.send('This is an owner only command. You cannot use this, sadly.');
+        const command = args.join(" ");
+        if (!command) return message.channel.send('You need to enter something for me to eval it!');
+        try {
+            const evaled = eval(command)
+
+            var embed = new MessageEmbed()
+                .setColor(client.color)
+                .setTitle('Evaluated')
+                .addFields(
+                    { name: 'To Eval', value: `\`\`\`${command}\`\`\`` },
+                    { name: 'Evaled', value: `\`\`\`JS\n${inspect(evaled, { depth: 0 })}\`\`\`` },
+                    { name: 'Type', value: `\`\`\`${typeof (evaled)}\`\`\`` }
+                )
+                .setTimestamp()
+            message.channel.send(embed);
+        } catch (error) {
+            var embed = new MessageEmbed()
+                .setColor(client.color)
+                .setTitle('Error')
+                .addFields(
+                    { name: 'Error', value: `${error}` }
+                )
+                .setTimestamp()
+            message.channel.send(embed);
+        }
+    }
+}
--- commands/topguilds.js
+++ commands/topguilds.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (message, args, client) => {
         if (message.author.id !== '594504468931018752') return message.channel.send('This Is A Owner Only Command. You Cannot Use This, Only The Owner Of The Bot Can!!! <:1099_chicken_nocie:793737126357368843>');
         const embed = new MessageEmbed()
-        const guilds = bot.guilds.cache
+        const guilds = client.guilds.cache
             .sort((a, b) => b.memberCount - a.memberCount)
             .first(10);
         const description = guilds
