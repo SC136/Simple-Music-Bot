3fb742c1f2bd58a8eb55ff6acabca74e475e8864
SC136 <swarchuri06@gmail.com>
2021-04-12 14:20:52 +0530

    Simple Music Bot

---
+++ commands/server.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'server',
+    run: async (message, args, client) => {
+        const embed = new MessageEmbed()
+            .setTitle('<:SimpleMusicBot:797533617042882612> Simple Music Bot Support Server Invite Link :')
+            .setThumbnail(`${client.user.displayAvatarURL()}`)
+            .setDescription('Click [Here](https://discord.gg/Qysc2PXp5e) To Join The Support Server')
+            .setColor("#2F3136")
+        message.channel.send(embed)
+    }
+}
---
+++ commands/servers.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'servers',
+    run: async (message, args, client) => {
+        const embed = new MessageEmbed()
+            .setTitle('<:SimpleMusicBot:797533617042882612> Simple Music Bot')
+            .setDescription('The Total Number Of \nDiscord Servers That The \nBot Is In')
+            .setThumbnail(`${client.user.displayAvatarURL()}`)
+            .addField('Total Servers :', `\`\`\`${client.guilds.cache.size}\`\`\``)
+            .setColor("#2F3136")
+            .setTimestamp()
+        message.channel.send(embed)
+    }
+}
