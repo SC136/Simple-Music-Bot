5b99c3c9f4ad547cab31418323ac809dd1735fdc
SC136 <swarchuri06@gmail.com>
2021-06-03 23:29:20 +0530

    Simple Music Bot

--- V3 Commands/Now Playing.js
+++ V3 Commands/Now Playing.js
@@ -10,9 +10,6 @@ module.exports = {
         if (!client.player.getQueue(message)) return message.channel.send(error.setDescription('```There Is No Music Currently Playing!```'));
 
         const track = client.player.nowPlaying(message);
-        //const filters = [];
-
-        //Object.keys(client.player.getQueue(message).filters).forEach((filterName) => client.player.getQueue(message).filters[filterName]) ? filters.push(filterName) : false;
 
         message.channel.send({
             embed: {
@@ -20,19 +17,20 @@ module.exports = {
                 author: { name: track.title },
                 footer: { text: 'Simple Music Bot V3' },
                 fields: [
-                    { name: 'Channel', value: track.author, inline: true },
-                    { name: 'Requested by', value: track.requestedBy.username, inline: true },
-                    { name: 'From playlist', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
+                    { name: 'Channel :', value: track.author, inline: true },
+                    { name: 'Requested By :', value: track.requestedBy.username, inline: true },
+                    { name: 'Playlist :', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
 
-                    { name: 'Views', value: track.views, inline: true },
-                    { name: 'Duration', value: track.duration, inline: true },
-                    { name: 'Filters activated', value: filters.length + '/' + client.filters.length, inline: true },
+                    { name: 'Views :', value: track.views, inline: true },
+                    { name: 'Duration :', value: track.duration, inline: true },
+                    { name: 'Song Link :', value: track.url, inline: true },
+                    { name: 'Source :', value: track.source, inline: true },
 
-                    { name: 'Volume', value: client.player.getQueue(message).volume, inline: true },
-                    { name: 'Repeat mode', value: client.player.getQueue(message).repeatMode ? 'Yes' : 'No', inline: true },
-                    { name: 'Currently paused', value: client.player.getQueue(message).paused ? 'Yes' : 'No', inline: true },
+                    { name: 'Volume :', value: client.player.getQueue(message).volume, inline: true },
+                    { name: 'Repeat Mode :', value: client.player.getQueue(message).repeatMode ? 'Yes' : 'No', inline: true },
+                    { name: 'Currently Paused :', value: client.player.getQueue(message).paused ? 'Yes' : 'No', inline: true },
 
-                    { name: 'Progress bar', value: client.player.createProgressBar(message, { timecodes: true }), inline: true }
+                    { name: 'Progress Bar :', value: client.player.createProgressBar(message, { timecodes: true }), inline: false }
                 ],
                 thumbnail: { url: track.thumbnail },
                 timestamp: new Date(),
