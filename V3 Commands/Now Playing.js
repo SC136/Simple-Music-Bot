04c833ed175fcd6bf627bc5bc6a16dadddc38b70
SC136 <swarchuri06@gmail.com>
2021-06-04 09:27:29 +0530

    Simple Music Bot

--- V3 Commands/Now Playing.js
+++ V3 Commands/Now Playing.js
@@ -10,30 +10,28 @@ module.exports = {
         if (!client.player.getQueue(message)) return message.channel.send(error.setDescription('```There Is No Music Currently Playing!```'));
 
         const track = client.player.nowPlaying(message);
-
+        const queue = client.player.getQueue(message);
         message.channel.send({
             embed: {
                 color: '#2F3136',
-                author: { name: track.title },
+                author: { name: Now Playing : },
+                title: { track.title },
                 footer: { text: 'Simple Music Bot V3' },
-                fields: [
-                    { name: 'Channel :', value: track.author, inline: true },
-                    { name: 'Requested By :', value: track.requestedBy.username, inline: true },
-                    { name: 'Playlist :', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
-
-                    { name: 'Views :', value: track.views, inline: true },
-                    { name: 'Duration :', value: track.duration, inline: true },
-                    { name: 'Song Link :', value: track.url, inline: true },
-                    { name: 'Source :', value: track.source, inline: true },
-
-                    { name: 'Volume :', value: client.player.getQueue(message).volume, inline: true },
-                    { name: 'Repeat Mode :', value: client.player.getQueue(message).repeatMode ? 'Yes' : 'No', inline: true },
-                    { name: 'Currently Paused :', value: client.player.getQueue(message).paused ? 'Yes' : 'No', inline: true },
-
-                    { name: 'Progress Bar :', value: client.player.createProgressBar(message, { timecodes: true }), inline: false }
-                ],
                 thumbnail: { url: track.thumbnail },
                 timestamp: new Date(),
+                fields: [
+                    { name: 'Channel :', value: `\`\`\`${track.author}\`\`\``, inline: true },
+                    { name: 'Requested By :', value: `\`\`\`${track.requestedBy.username}\`\`\``, inline: true },
+                    { name: 'Playlist :', value: `\`\`\`${track.fromPlaylist ? 'Yes' : 'No'}\`\`\``, inline: true },
+                    { name: 'Views :', value: `\`\`\`${track.views}\`\`\``, inline: true },
+                    { name: 'Duration :', value: `\`\`\`${track.duration}\`\`\``, inline: true },
+                    { name: 'Song Link :', value: `[URL](${track.url})`, inline: true },
+                    { name: 'Source :', value: `\`\`\`${track.source}\`\`\``, inline: true },
+                    { name: 'Volume :', value: `\`\`\`${queue.volume}\`\`\``, inline: true },
+                    { name: 'Repeat Mode :', value: `\`\`\`${queue.repeatMode ? 'Yes' : 'No'}\`\`\``, inline: true },
+                    { name: 'Currently Paused :', value: `\`\`\`${queue.paused ? 'Yes' : 'No'}\`\`\``, inline: true },
+                    { name: 'Progress Bar :', value: `\`\`\`${client.player.createProgressBar(message, { timecodes: true })}\`\`\``, inline: false }
+                ],
             },
         });
     },
--- V3 Commands/Queue.js
+++ V3 Commands/Queue.js
@@ -10,8 +10,19 @@ module.exports = {
 
         if (!client.player.getQueue(message)) return message.channel.send(error.setDescription('```There Are No Songs Currently Playing!```'));
 
-        message.channel.send(`**Server queue - ${message.guild.name} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
+//        message.channel.send(`**Server queue - ${message.guild.name} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
+//            return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
+//        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
+
+        message.channel.send({
+            embed: {
+                author: { name: 'Music Queue For :' },
+                title: { message.guild.name },
+                field: { 'Current Song :', `[${queue.playing.title} : ${queue.playing.author}](${queue.playing.url})` },
+                description: { (queue.tracks.map((track, i) => {
             return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
-        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
+        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`) }
+            }
+        })
     }
 }
